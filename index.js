ca5a27e829c61aade3f0bd8c3637c8a0ea6054b7
Zachary <zachary.zboncak@gmail.com>
2019-09-17 00:29:09 -0500

    added function stubs and basic styling

---
+++ index.js
@@ -0,0 +1,26 @@
+//globals
+let score = 0;
+let num1;
+let num2;
+
+function generateNumbers() {
+    //this function will generate two random numbers to appear in the h2 tag
+    num1 = Math.floor(Math.random()*10);
+    num2 = Math.floor(Math.random()*10);
+    console.log(`generateNumbers ran!`);
+}
+
+function evaluateInput() {
+    //this function will evaluate if the user's input is the sum of the two numbers.
+    console.log(`evaluateInput ran!`);
+}
+
+function renderResults() {
+    //this function will display in the results section if the input was correct or incorrect
+    console.log(`renderResults ran!`);
+}
+
+$('#generate').click(generateNumbers);
+
+//when the page loads random numbers will be generated to display
+generateNumbers();
