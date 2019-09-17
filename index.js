b2838445de64bd62fac6f7d827c3b115f3c7207b
Zachary <zachary.zboncak@gmail.com>
2019-09-17 00:42:41 -0500

    completed basic functions

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let score = 0;
 let num1;
 let num2;
+let correctAnswer;
 
 function generateNumbers() {
     //this function will generate two random numbers to appear in the h2 tag
@@ -12,19 +13,31 @@ function generateNumbers() {
     $('#num2').text(num2);
     //console.log(num2);
     //console.log(`generateNumbers ran!`);
+    correctAnswer = num1 + num2;
 }
 
 function evaluateInput() {
     //this function will evaluate if the user's input is the sum of the two numbers.
+    event.preventDefault;
+    let userAnswer = $('#number').val();
+    if (userAnswer == correctAnswer) {
+        renderResults("Yes!");
+    }
+    else {
+        renderResults("Uh, oh. Try again!");
+    }
     console.log(`evaluateInput ran!`);
 }
 
 function renderResults(result) {
     //this function will display in the results section if the input was correct or incorrect
-    
+    $('.feedback').text(result);
     console.log(`renderResults ran!`);
 }
 
+$('#submit').click(evaluateInput);
+
+//generates a new number when the button is clicked
 $('#generate').click(generateNumbers);
 
 //when the page loads random numbers will be generated to display
